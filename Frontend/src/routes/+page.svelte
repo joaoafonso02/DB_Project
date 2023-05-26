31daa09579f30ddcc8907d59eed4203178783a4e
João Afonso <ferreiraafonsojoao@ua.pt>
2023-05-26 17:50:06 +0100

    frontend

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import PhoneAppLayout from './PhoneAppLayout.svelte';
+	import { onMount } from 'svelte';
+  
+	let title = 'Welcome to TrotiNet';
+  
+	onMount(() => {
+	  document.title = title;
+	});
+  </script>
+  
+  <PhoneAppLayout {title}>
+	<img src="/trotinet.jpg" alt="TrotiNet logo" class="mx-auto mt-3" style="width: 100%; height: auto;" />
+	<div class="container" style="max-width: 300px;">
+	  <button type="button" class="btn btn-outline-primary btn-lg mt-3 w-100" on:click={()=>window.location.href='/login'}>Login/Register</button>
+	</div>
+  </PhoneAppLayout>
+  
---
+++ Frontend/src/routes/login/+page.svelte
@@ -0,0 +1,147 @@
+<script>
+    import PhoneAppLayout from '../PhoneAppLayout.svelte';
+    import { onMount } from 'svelte';
+
+    let title = 'Login';
+
+    onMount(() => {
+      document.title = title;
+    });
+
+    let credentials = {usr: "admin", password: "1234"};
+
+    function submitSignin(){
+        let usr = document.querySelector("#formSigninUsername").value;
+        let password = document.querySelector("#formSigninPassword").value;
+        const users = credentials;
+        console.log(credentials.length);
+            console.log("EW");
+            if(users['usr'] == usr && users['password'] == password){
+                window.location.href='/';
+            }
+            else {
+                showAlert("Unknown username or password, try again!");
+            }
+    }
+
+    let message = "";
+
+
+    function showAlert(msg) {
+        setTimeout(() => {
+            message = "";
+        }, 3000);
+        message = msg;
+    }
+
+</script>
+
+<PhoneAppLayout {title}>
+  <section class="text-center text-lg-start">
+ 
+      {#if message}
+        <div class="alert">{message}</div>
+      {/if}
+      <br>
+      <!-- Jumbotron -->
+      <div class="row g-0 align-items-center justify-content-center" >
+        
+        <div class="card cascading-right border-0">
+            <div class="card-body p-5 shadow-4 text-center">
+              <!-- Pills navs -->
+              <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link active" id="tab-login" data-bs-toggle="pill" data-bs-target="#pills-login" type="button" role="tab" aria-controls="pills-login" aria-selected="true">Login</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="tab-register" data-bs-toggle="pill" data-bs-target="#pills-register" type="button" role="tab" aria-controls="pills-register" aria-selected="false">Register</button>
+                </li>
+              </ul>
+              <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane show active" id="pills-login" role="tabpanel" aria-labelledby="pills-login">
+                  <form>
+                    <br>
+                    <!-- Username input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="loginName">Username</label>
+                      <input id="formSigninUsername" type="text" class="form-control" style="text-align: center;">
+                    </div>
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="loginPassword">Password</label>
+                      <input id="formSigninPassword" type="password" class="form-control" style="text-align: center;">
+                    </div>
+                    <!-- 2 column grid layout -->
+                    <div class="row mb-4">
+                      <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check mb-3 mb-md-0">
+                          <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked>
+                          <label class="form-check-label" for="loginCheck"> Remember me </label>
+                        </div>
+                      </div>
+                      <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Simple link -->
+                        <a href="#!">Forgot password?</a>
+                      </div>
+                    </div>
+                    <!-- Submit button -->
+                    <div id="formSignin" onsubmit="submitSignin()">
+                      <button type="submit" class="btn btn-primary btn-block mb-4" on:click={submitSignin}>Sign in</button>
+                    </div>
+                    <!-- Register buttons -->
+                    <div class="text-center">
+                      <p>Not a member? <a href="#pills-register" data-bs-target="#pills-register" id="registerBtn">Register</a></p>
+                    </div>
+                  </form>
+                </div>
+                <div class="tab-pane" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
+                  <form>
+                    <!-- Username input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label mt-3" for="registerUsername">Username</label>
+                      <input id="formSignupUsername" type="text" class="form-control">
+                    </div>
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="registerEmail">Email</label>
+                      <input id="formSignupEmail" type="email" class="form-control">
+                    </div>
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="registerPassword">Password</label>
+                      <input id="formSignupPassword1" type="password" class="form-control">
+                    </div>
+                    <!-- Repeat Password input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+                      <input id="formSignupPassword2" type="password" class="form-control">
+                    </div>
+                    <!-- Checkbox -->
+                    <div class="form-check d-flex justify-content-center mb-4">
+                      <input id="formSignupCheck" class="form-check-input me-2" type="checkbox" value="" checked aria-describedby="registerCheckHelpText">
+                      <label class="form-check-label" for="registerCheck">I have read and agree to the terms</label>
+                    </div>
+                    <!-- Submit button -->
+                    <button id="formSignup" class="btn btn-primary btn-block mb-3">Register</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+      <!-- Jumbotron -->
+  </section>
+</PhoneAppLayout>
+
+<style>
+  .alert {
+    background-color: red;
+    color: white;
+    text-align: center;
+    padding: 10px;
+    margin-top: 10px;
+    width: 100%;
+  }
+</style>
